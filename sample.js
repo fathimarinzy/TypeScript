ae26664d81dd00d379b2f908d134dc912f65360d
fathimarinzy <fathimarinzy02@gmail.com>
2025-01-13 22:41:29 +0530

    Typescript installing

---
+++ sample.js
@@ -0,0 +1,14 @@
+console.log("welcome to typescript");
+// datatypes
+var a1 = 10;
+console.log(a1);
+var b = "hello";
+console.log(b);
+var c = true;
+console.log(c);
+var a2 = 10;
+console.log(a2);
+var b2 = "hello";
+console.log(b2);
+var c2 = true;
+console.log(c2);
---
+++ sample.ts
@@ -0,0 +1,20 @@
+console.log("welcome to typescript")
+
+// datatypes
+let a1:number=10;
+console.log(a1)
+
+let b:string="hello";
+console.log(b)
+
+let c:boolean=true;
+console.log(c)
+
+let a2:any=10;
+console.log(a2)
+
+let b2:any="hello";
+console.log(b2)
+
+var c2:any=true;
+console.log(c2)
