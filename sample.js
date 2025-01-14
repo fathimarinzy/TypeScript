2114eaae90c87b42ea3f4a956ddb3d27ec38f0ef
fathimarinzy <fathimarinzy02@gmail.com>
2025-01-14 14:39:36 +0530

    oops in typescript

--- sample.js
+++ sample.js
@@ -12,3 +12,58 @@ var b2 = "hello";
 console.log(b2);
 var c2 = true;
 console.log(c2);
+// arrays
+var fruits = ['apple', 'orange', 'banana'];
+console.log(fruits);
+var fruit = ['apple', 'orange', 'banana'];
+console.log(fruit[0]);
+// iterate
+for (var _i = 0, fruits_1 = fruits; _i < fruits_1.length; _i++) {
+    var i = fruits_1[_i];
+    console.log(i);
+}
+for (var i in fruits) {
+    console.log(i);
+}
+// objects
+var per = { "name": 'rinzy', "age": 23 };
+console.log(per);
+console.log(per.name);
+console.log(per.age);
+for (var i_1 in per) {
+    console.log(per);
+    console.log(per[i_1]);
+}
+// oops
+var Student = /** @class */ (function () {
+    function Student() {
+    }
+    Student.prototype.show = function () {
+        console.log(this.name + " " + this.age + " " + this.course);
+    };
+    return Student;
+}());
+var obj1 = new Student();
+obj1.name = "ammu";
+obj1.age = 10;
+obj1.course = "python";
+obj1.show();
+//  constructor case
+var Student1 = /** @class */ (function () {
+    function Student1(name, age, course) {
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+    Student1.prototype.show = function () {
+        console.log(this.name + " " + this.age + " " + this.course);
+    };
+    Student1.prototype.displayage = function () {
+        return this.age;
+    };
+    return Student1;
+}());
+var obj2 = new Student1("ammu", 10, "python");
+obj1.show();
+var x = obj2.displayage();
+console.log(x);
--- sample.ts
+++ sample.ts
@@ -18,3 +18,68 @@ console.log(b2)
 
 var c2:any=true;
 console.log(c2)
+
+// arrays
+let fruits:string[]=['apple','orange','banana'];
+console.log(fruits)
+
+let fruit:Array<string>=['apple','orange','banana'];
+console.log(fruit[0])
+
+// iterate
+for (var i of fruits){
+    console.log(i)
+}
+for (var i in fruits){
+    console.log(i)
+}
+
+// objects
+let per={"name":'rinzy',"age":23};
+console.log(per)
+console.log(per.name)
+console.log(per.age)
+for (let i in per){
+    console.log(per)
+    console.log(per[i])
+
+}
+
+// oops
+class Student{
+    name:string;
+    age:number;
+    course:string;
+    show():void{
+        console.log(this.name+" "+this.age+" "+this.course)
+    }
+ }
+ var obj1=new Student()
+ obj1.name="ammu";
+ obj1.age=10;
+ obj1.course="python"
+ obj1.show()
+
+
+//  constructor case
+
+class Student1{
+    name:string;
+    age:number;
+    course:string;
+    show():void{
+        console.log(this.name+" "+this.age+" "+this.course)   
+    }
+    constructor(name:string,age:number,course:string){
+        this.name=name;
+        this.age=age;
+        this.course=course;
+    }
+    displayage():number{
+        return this.age
+    }
+}
+var obj2=new Student1("ammu",10,"python")
+obj1.show()
+var x=obj2.displayage()
+console.log(x)
